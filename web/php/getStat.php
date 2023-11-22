d7e87c18c07bdce61bcf96c75d374f7146728831
erkekeke <baxaisov.erke@yandex.ru>
2023-11-22 15:00:28 +0200

    Починил ось времени на php getStat.php

--- web/php/getStat.php
+++ web/php/getStat.php
@@ -118,7 +118,7 @@
         var myChart = new Chart(ctx, {
             type: 'line', // Используем scatter plot для показа зависимости
             data: {
-                labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+                labels: elapsedTime,
                 datasets: [{
                     label: 'Зависимость затраченного времени от размера директории',
                     data: data.size,
